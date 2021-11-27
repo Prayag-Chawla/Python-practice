ded35033524f87a8761134d03a109680c6ac3b29
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2021-11-27 09:34:03 +0530

    Add files via upload

---
+++ Heartt.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.bgcolor("black")
+
+turtle.pensize(10)
+turtle.speed(2)
+turtle.color('red')
+turtle.begin_fill()
+turtle.fillcolor("red")
+turtle.left(150)
+turtle.forward(180)
+turtle.circle(-90,180)
+turtle.setheading(60)
+turtle.circle(-90,180)
+turtle.forward(180)
+turtle.end_fill()
+turtle.mainloop()
+turtle.done()
