2e5eba1a581de3de5ef29f866988280a7fcd1c2d
Prayag Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-07-21 18:14:16 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,7 @@
+n1 = int (input("Enter marks obtained in the first test: "))
+n2 = int (input("Enter marks obtained in the second test: "))
+n3 = int (input("Enter marks obtained in third test: "))
+total = n1+n2+n3
+
+Avg = total / 3
+print ("The average marks of the student is : ",Avg)
