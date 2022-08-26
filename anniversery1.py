7d8134fcc6ccb933a7249fa3d7918987ed9a4cf5
Prayag Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-08-26 22:54:35 +0530

    Add files via upload

---
+++ anniversery1.py
@@ -0,0 +1,16 @@
+import qrcode  
+ 
+obj_qr = qrcode.QRCode(  
+    version = 1,  
+    error_correction = qrcode.constants.ERROR_CORRECT_L,  
+    box_size = 10,  
+    border = 4,  
+)  
+ 
+obj_qr.add_data("https://writing-passion-tolivelife.blogspot.com/2022/04/when-everything-falls-apart-parents-are.html")  
+  
+obj_qr.make(fit = True)  
+  
+qr_img = obj_qr.make_image(fill_color = "cyan", back_color = "black")  
+  
+qr_img.save("qr-img1.png")  
